5530cef4001f4ae3b413f0fe331f7e7071c51cce
LukaLiegis <luka.liegis@gmail.com>
2024-03-02 09:51:24 +0200

    Commit binomial-tree.py and black-scholes.py

---
+++ models/black-scholes.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def black_scholes(S, K, T, r, sigma):
+    """
+    :param S:
+    :param K:
+    :param T:
+    :param r:
+    :param sigma:
+    :return:
+    """
+
+    d1 =
+    d2 =
+    call_price =
+    put_price = 
